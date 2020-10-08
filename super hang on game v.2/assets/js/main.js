a038715b3f5dc4d05d28159b37727a6d0a2ff05d
jarmokanon <62425744+jarmokanon@users.noreply.github.com>
2020-10-08 22:04:27 +0200

    deadline 9 oktober

--- super hang on game v.2/assets/js/main.js
+++ super hang on game v.2/assets/js/main.js
@@ -66,8 +66,8 @@ window.addEventListener(
         canvas.style.backgroundColor = 'black';
         document.getElementById('container').appendChild(canvas);
 
-        ctx = canvas.getContext('2d', { alpha: false });
-        ctx.imageSmoothingEnabled = false;
+        ctx = canvas.getContext('2d');
+        //ctx.imageSmoothingEnabled = false;
         ctx.font = '20px Verdana';
 
         Keyboard.setHandlers();
